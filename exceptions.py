33fb470a7c86faa30b9be80cadba0e5367adde2c
Materij <48157664+Materij@users.noreply.github.com>
2022-04-29 17:08:54 +0300

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -3,11 +3,11 @@ class BoardException(Exception):
 
 class BoardOutException(BoardException):
     def __str__(self):
-        return "Вы пытаетесь выстрелить в доску"
+        return "You try to shoot the board"
 
 class BoardUsedException(BoardException):
     def __str__(self):
-        return "Вы уже стреляли в эту клетку"
+        return "You already shot that cage"
 
 class BoardWrongShipException(BoardException):
     pass
